0decc8a5222672cc8f51f34b9e24b082ab202e20
Danakwt <66743842+Danakwt@users.noreply.github.com>
2020-06-16 15:11:08 +0300

    تم الحل

---
+++ CW-DanaAlmunaikh-197.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+//PART 1
+let name = "Dana almunaikh"
+var age = 16
+var height = 1.6
+var Hobby1 = "Horse Riding"
+var Hobby2 = "Reading"
+
+print(name)
+print(age)
+print(height)
+print(Hobby1)
+print(Hobby2)
+
+print("my name is \(name), i am \(age) year old, my hobbies are \(Hobby1) and \(Hobby2)" )
+
+//PART2
+let Friendname = "Jenan"
+var Friendage = 16
+var Friendheight = 1.55
+var FriendHobby1 = "Swimming"
+var FriendHobby2 = "Reading"
+
+var age_difference = age - Friendage
+
+name == Friendname
+name != Friendname
+name < Friendname
+
+age == Friendage
+age != Friendage
+age < Friendage
+
+height += Friendheight
+height != Friendheight
+height -= Friendheight
+
+Hobby2 == FriendHobby2
+Hobby2 != FriendHobby2
+Hobby1 == FriendHobby1
